7d78222bb3011a361a0686861a96efe9e8cc6c1f
Unknown <hugues.vogel@hotmail.fr>
2018-05-02 07:55:27 +0200

    wip

--- src/app/pages/page-basket/page-basket.component.ts
+++ src/app/pages/page-basket/page-basket.component.ts
@@ -17,7 +17,32 @@ export class PageBasketComponent implements OnInit {
   infoArticles: { article: Article, magasin: Magasin }[];
   dateLivraison;
 
-  msgError(msg: string) {
+  paypalCfg = {
+
+    env: 'sandbox', // 'production' or 'sandbox'
+
+    commit: true, // Show a 'Pay Now' button
+
+    payment: function() {
+      return paypal.request.post(CREATE_PAYMENT_URL).then(function(data) {
+        return data.id;
+      });
+    },
+
+    onAuthorize: function(data) {
+      return paypal.request.post(EXECUTE_PAYMENT_URL, {
+        paymentID: data.paymentID,
+        payerID:   data.payerID
+      }).then(function() {
+
+        // The payment is complete!
+        // You can now show a confirmation message to the customer
+      });
+    }
+
+  };
+
+  msgError(msg: string){
     console.log(msg);
   }
 
