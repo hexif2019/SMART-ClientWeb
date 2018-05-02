e3e792cc25042045723d47c7b53ad85beb1397b5
Unknown <hugues.vogel@hotmail.fr>
2018-05-02 17:45:39 +0200

    fix residance bug refacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserService } from './services/user.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ResidanceService} from './services/residence.service';
+import { ResidenceService} from './services/residence.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageShopComponent } from './pages/page-shop/page-shop.component';
@@ -64,7 +64,7 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [UserService, ResidanceService, PanierService, CommandeService],
+  providers: [UserService, ResidenceService, PanierService, CommandeService],
   bootstrap: [AppComponent],
   exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
